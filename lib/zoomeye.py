992d7799f5c3564056a77ad86ba9c4be3d9380f1
spmonkey <35283200+spmonkey@users.noreply.github.com>
2022-01-20 09:21:49 +0800

    Update zoomeye.py

--- lib/zoomeye.py
+++ lib/zoomeye.py
@@ -2,7 +2,7 @@
 import requests, tqdm
 import re, os, shutil
 
-headers = {'API-KEY': '输入你自己的API-KEY'}
+headers = {'API-KEY': '输入您自己的API-KEY'}
 search_info_num = []
 # 用户
 '''
@@ -14,7 +14,6 @@ remain_total_quota : 剩余总额度
 def user_info():
     url_info = 'https://api.zoomeye.org/resources-info'  # 获取资源信息：用户基本信息及可用额度
     resources_info = requests.get(url=url_info, headers=headers)
-    # print(resources_info.text)
     search_info = re.findall('"remain_total_quota": (.+).+.+', resources_info.text)  # 获取总额度
     for remain in search_info:
         search_info_num.append(remain)
@@ -34,23 +33,24 @@ def webscan():
         print(remain_total_quota)
         print("可以进行下一步操作！")
         page = int(input("请输入需要爬取的页数："))
-        for i in range(page):
-            web_scan = 'https://api.zoomeye.org/web/search?'
-            query = 'query=' + input("""
+        web_scan = 'https://api.zoomeye.org/web/search?'
+        query = 'query=' + input("""
 请输入查询关键词(例子 : app:"DedeCMS" +country:"CN")：
-            """)
+        """)
+        facets = 'facets=' + input("""
+        请输入统计项(用,隔开，例子：webapp,os)：
+                    """)
+        for i in range(1, page+1):
             pages = 'page=' + str(i)
-            facets = 'facets=' + input("""
-请输入统计项(用 , 隔开)：
-            """)
             urls = web_scan + query + '&' + pages + '&' + facets
             webscan = requests.get(url=urls,headers=headers)
             webscan.encoding = 'utf-8'
-            # print(webscan.text)
-            isExists = os.path.exists('webscan')
-            if isExists == True:
-                shutil.rmtree('webscan')
-                os.mkdir('webscan')
+            isExists_dir = os.path.exists('webscan')
+            isExists_file = os.path.exists('webscan/webscan{0}.json'.format(i))
+            if isExists_dir == True:
+                if isExists_file == True:
+                    os.remove('webscan/webscan{0}.json'.format(i))
+                pass
             else:
                 os.mkdir('webscan')
             with open('webscan/webscan{0}.json'.format(i), 'a+', encoding='utf-8') as w:
@@ -67,26 +67,30 @@ def domain():
     remain_total_quota = int(search_info_num[0])
     if int(remain_total_quota) > 0:
         print("可以进行下一步操作！")
-        for i in range(1, 5):
-            domain_scan = 'https://api.zoomeye.org/domain/search?'
-            q = 'q=' + input("""
+        page = int(input("请输入需要爬取的页数："))
+        domain_scan = 'https://api.zoomeye.org/domain/search?'
+        q = 'q=' + input("""
 请输入查询关键字(例子：baidu.com)：
-            """)
-            page = 'page=' + str(i)
-            type = 'type=' + input("""
+        """)
+        type = 'type=' + input("""
 请输入类型(默认为 0 )：
-            """)
+                            """)
+        for i in range(1, page+1):
+            page = 'page=' + str(i)
             urls = domain_scan + q + '&' + page + '&' + type
             domainscan = requests.get(url=urls,headers=headers)
-            isExists = os.path.exists('domainscan%d'%i)
-            if isExists == True:
-                shutil.rmtree('domainscan%d'%i)
-                os.mkdir('domainscan%d'%i)
+            isExists_dir = os.path.exists('domainscan')
+            isExists_file = os.path.exists('domainscan/domainscan{0}.json'.format(i))
+            if isExists_dir == True:
+                if isExists_file == True:
+                    os.remove('domainscan/domainscan{0}.json'.format(i))
+                pass
             else:
-                os.mkdir('domainscan%d'%i)
-            with open('./domainscan{0}/./domainscan{0}.txt'.format(i), 'a+', encoding='utf-8') as w:
+                os.mkdir('domainscan')
+            with open('domainscan/domainscan{0}.json'.format(i), 'a+', encoding='utf-8') as w:
                 w.write(domainscan.text)
         return "主机资产收集完成！请开始您的渗透之旅吧~"
     else:
         return "您的额度不够，请充值或更换账号！"
         pass
+    
